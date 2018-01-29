3c803e779a55b678d70a2a57bdf752a17be3c125
user-PC\user <vesta.nagase@gmail.com>
2018-01-29 19:03:40 +0900

    <!DOCTYPE html>削除

---
+++ lp/index.php
@@ -0,0 +1,374 @@
+
+<html lang="ja-JP">
+<head>
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<!--[if IE]>
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+	<![endif]-->
+	<meta http-equiv="content-language" content="ja">
+	<meta charset="UTF-8">
+	<meta name="keywords" content="職業訓練,ハロワ,ハローワーク,WEB,ホームページ作成,スマートフォン,デザイナー,スクール,訓練校,スマホアプリ,Java Script,アンドロイド,Android,iPhone,iOS,開発,ネットショップ,ワードプレス,WordPress,Web,ウェブ">
+	<meta name="description" content="【求職者支援訓練】2018年3月開講　訓練生募集！初心者から経験者まで、転職・就職・再就職に役立つスキルが身につきます。「iPhoneアプリを作ってみたい」「Androidアプリを作ってみたい」「プログラマーになりたい」方、HITパソコンスクールでプロへの道を目指しませんか？インターン制度があるので、詳細はお問い合わせください。">
+	<title>【求職者支援訓練】2018年3月開講 訓練生 募集！</title>
+	<link rel="stylesheet" href="css/style.css">
+	<script src="//code.jquery.com/jquery-3.2.1.min.js"></script>
+	<script>
+	// 設定(講座名)
+	var KOUZA = 'WEBクリエイター・デザイン実践科';
+	var KOUZA2 = 'ネットショップ・Ｗｅｂ 制作スタッフ養成科';
+	var KOUZA3 = 'スマートフォン（Android）アプリ開発科';
+	// 設定(開講年月)
+	var YEAR = '2018';
+	var KAIKOU = '3';
+	var KAIKOU_ZEN = '16';
+	var KIKAN_ZEN = '4';
+	var KIKAN_ZEN2 = '5';
+	var KIKAN_ZEN3 = '5';
+	// 設定(募集期間)
+	var BOSYU_HAJIME_HAN = '1月25日';
+	var BOSYU_OWARI_HAN = '2月15日';
+	var BOSYU_HAJIME_ZEN = '１月２５日';	// 全角
+	var BOSYU_OWARI_ZEN = '２月１５日';	// 全角
+	var COUNTDOWN = '2018/2/15 17:00:00';
+	var MSG1 = KAIKOU + '月開講募集';
+	var MSG2 = YEAR + '年' + KAIKOU + '月';
+	var MSG3 = KAIKOU + '月';
+	var MSG4 = '2/15';
+	var MSG5 = KAIKOU + '月開講';
+
+	$(function(){
+		$('.kouza1').text(KOUZA);
+		$('.kouza2').text(KOUZA2);
+		$('.kouza3').text(KOUZA3);
+		$('.kaikou').text(KAIKOU);
+		$('.kaikou-zen').text(KAIKOU_ZEN);
+		$('.kikan-zen').text(KIKAN_ZEN);
+		$('.kikan-zen2').text(KIKAN_ZEN2);
+		$('.kikan-zen3').text(KIKAN_ZEN3);
+		$('.bosyu-hajime-han').text(BOSYU_HAJIME_HAN);
+		$('.bosyu-owari-han').text(BOSYU_OWARI_HAN);
+		$('.bosyu-hajime-zen').text(BOSYU_HAJIME_ZEN);
+		$('.bosyu-owari-zen').text(BOSYU_OWARI_ZEN);
+		$('.msg1').text(MSG1);
+		$('.msg2').text(MSG2);
+		$('.msg3').text(MSG3);
+		$('.msg4').text(MSG4);
+		$('.msg5').text(MSG5);
+	});
+	</script>
+	<script type="text/javascript" src="js/base.js"></script>
+    <style>
+
+    </style>
+</head>
+<body>
+	<div id="page-contents">
+		<div id="main-image">
+			<img src="images/main.png" alt="本気で就職・再就職を目指すなら、新しい技術を身につけ自分の強みを増やせるHITパソコンスクールでスキルアップを！">
+			<div id="advertisement-content">
+
+				<span class="msg1"></span>
+				<span class="msg4"></span>
+				<span class="msg5" style="font-size:22px;"></span>
+
+				<img class="advertisement" src="images/advertisement.png" alt="[求職者支援訓練]2018年3月開講　訓練生募集！無料で学べ、ITスキルが身につくとってもお得な訓練です。この機会に是非スキルアップをしてください">
+				<img class="rimit" src="images/rimit.png" alt="募集締め切り：2月15日まで！期間が短いためお急ぎください。住所地を管轄するハローワークでお申し込みください">
+				<a href="tel:06-6940-0566"><img class="tel-bt" src="images/tel-bt.png" alt="求職者支援訓練に関するお問い合わせはこちらから"></a>
+				<a href="#form"><img class="inquiry-bt" src="images/inquiry-bt.png" alt="求職者支援訓練に関するお問い合わせはこちらから"></a>
+			</div>
+		</div>
+		<h1 class="mobile-content">
+			【求職者支援訓練】<span class="kaikou"></span> 月開講募集期間：<br><span class="bosyu-hajime-han"></span>から<span class="bosyu-owari-han"></span>まで
+		</h1>
+
+		<!--コース内容　START-->
+
+		<dl class="course mtop">
+		<dt class="designer">WEBデザイナー志望のあなたには</dt>
+			<dd class="designer-content">
+				<dl>
+					<dt>
+						<p>
+							<span>実践コース&nbsp;　<span class="kikan-zen"></span>　ヶ月</span>（定員：8名）
+						</p>
+            <h2 style="margin-bottom:20px;"><span class="kouza1"></span></h2>
+					</dt>
+					<dd style="line-height:1.8rem;">
+						WEB制作は初めて...という方でも、基本を繰り返す事で、操作に慣れていく実践型のカリキュラムに実績があります。訓練修了後には「自信」をつけてお仕事に挑んでいただきます。
+				</dl>
+			</dd>
+		</dl>
+		
+		<dl class="course mtop">
+		<dt class="designer">ネットショップ運営志望のあなたには</dt>
+			<dd class="designer-content">
+				<dl>
+					<dt>
+						<p>
+							<span>実践コース&nbsp;　<span class="kikan-zen"></span>　ヶ月</span>（定員：18名）
+						</p>
+            <h2 style="margin-bottom:20px;"><span class="kouza2"></span></h2>
+					</dt>
+					<dd style="line-height:1.8rem;">
+						ネットショップの仕組み・運営・構築に関する知識を学べ、Web のデザイン・制作のスキルが身につきます。
+						オフィスソフトの基本操作も身につけれる盛りだくさんなコースになっています。
+				</dl>
+			</dd>
+		</dl>
+		<dl class="course mtop">
+		<dt class="programmer">プログラマー志望のあなたには</dt>
+			<dd class="programmer-content">
+				<dl>
+					<dt>
+						<p>
+							<span>実践コース&nbsp;　<span class="kikan-zen"></span>　ヶ月</span>（定員：15名）
+						</p>
+						<h2 style="margin-bottom:20px;"><span class="kouza3"></span></h2>
+					</dt>
+					<dd style="line-height:1.8rem;">
+						この講座では、開発未経験の方でもステップ・バイ・ステップでアプリ開発に必要な技術をしっかり学ぶ事ができます。また、求人のニーズが非常に高いプログラマーに必要な知識が実践的に学べるカリキュラムとなっております。
+					</dd>
+				</dl>
+			</dd>
+		</dl>
+
+		<!--<dl class="course">
+			<dt class="programmer" style="background:none!important;">基礎的ITスキルを学び、就職に活かしたいあなたには</dt>
+			<dd class="programmer-content">
+				<dl>
+					<dt>
+						<p>
+							<span>実践コース&nbsp;<span class="kikan-zen2"></span>ヶ月</span>駅近！徒歩約5分！！基礎を固め、次なるステップへ
+						</p>
+						<h2 style="margin-bottom:20px;"><span class="kouza2">ＯＡ事務スタッフ養成（夜間）科</span>（定員：25名）</h2>
+					</dt>
+					<dd style="line-height:1.8rem;">
+						パソコンが苦手な方、就職活動が上手くいかない方、MicrosoftOfficeのソフト操作に自信のない方などなど、事務職で求められるスキルをHITパソコンスクールで高めてみませんか？<br>
+						また、IT系のスクールだからこそ学べるネットワークの知識や、セキュリティについて、少しハイレベルなVBAやACCESSも学べるので、転職に有利！<br>
+						<span style="display:block; padding-top:12px;">訓練のお申し込みは<strong style="color:red;">管轄のハローワーク</strong>まで</span>
+
+					</dd>
+				</dl>
+			</dd>
+		</dl>-->
+
+		<!--コース内容　END-->
+
+		<div id="material">
+			<p>
+				<span style="display:block; 	padding-top:12px;">訓練のお申し込みは<strong style="color:red;">
+					管轄のハローワーク</strong>まで。
+				</span>
+			</p>
+			<p>
+				訓練の詳しいカリキュラム内容はPDFでご確認いただけます。
+			</p>
+			<table class="course-tb">
+				
+				<tr>
+					<th><span class="kouza1"></span></th>
+					<td><a href="pdf/201803_5481.pdf" target="_blank"><img src="images/dw-icon.jpg" alt="WEBクリエイター・デザイン実践科"></a></td>
+				</tr>
+				<tr>
+					<th><span class="kouza2"></span></th>
+					<td><a href="pdf/201803_5480.pdf" target="_blank"><img src="images/dw-icon.jpg" alt="ネットショップ・Ｗｅｂ 制作スタッフ養成科"></a></td>
+				</tr>
+				<tr>
+					<th><span class="kouza3"></span></th>
+					<td><a href="pdf/201803_5479.pdf" target="_blank"><img src="images/dw-icon.jpg" alt="スマートフォン（Android）アプリ開発科"></a></td>
+				</tr>
+			</table>
+			<p>
+				<small>PDF形式のファイルをご覧いただく場合には、Adobe Readerが必要です。Adobe Readerをお持ちでない方はバナーのリンク先からダウンロードしてください。</small><br>
+				<a href="http://get.adobe.com/jp/reader/" target="_blank"><img src="images/get_adobe_reader.png" alt=""></a>
+			</p>
+		</div>
+		<div id="about-training">
+			<h3>求職者支援制度とは</h3>
+			<p>
+			求職中の方が受講できる、厚生労働省の無料の教育訓練で、	本校ではITやWEB分野を目指している方の就職支援を目的とするものです。<a href="http://www.mhlw.go.jp/seisakunitsuite/bunya/koyou_roudou/koyou/kyushokusha_shien/index.html" target="_blank">詳しくはこちら→</a>
+			</p>
+			<h3>受講できる対象者</h3>
+			<p>
+			雇用保険未加入の方ならばどなたでも受講可能です。	但し、過去受講された方は受講できないなど一部制限もあります。<a href="http://www.mhlw.go.jp/bunya/koyou/kyushokusha_shien/dl/syuuchi-01.pdf" target="_blank">詳しくはこちら→</a>
+			</p>
+			<h3>給付金制度あり（月額１０万）</h3>
+			<p>
+			条件により、受講期間中に給付金が支給される制度があり、交通費も支給されます。詳しくは住所地を管轄するハローワークまでお問い合わせください。
+			</p>
+		</div>
+		<div class="inquiry-box">
+			<div class="inquiry-img">
+			<a href="#form"><img class="inquiry-bt" src="images/inquiry-bt.png" alt="求職者支援訓練に関するお問い合わせはこちらから"></a>
+			</div>
+			<p>募集期間：２０１８年<span class="bosyu-hajime-zen"></span>&nbsp;〜&nbsp;２０１８年<span class="bosyu-owari-zen"></span>まで</p>
+		</div>
+
+		<!-- セミナー案内開始
+		<div style="margin: 0 0 30px 40px;">
+			<img src="images/inquiry-busi.png" style="margin-left: -30px">
+			<p style="font-weight: bold; color: #ff0000; margin: 0 0 20px;">『 受講したいけどまとまった時間が取れない・・・』</p>
+			<p>そういった方にも受講可能なセミナー講座を開講しております。</p>
+			<p>カリキュラムなど、詳しくは下記リンクをクリックしてください。</p>
+			<p style="margin: 20px 0 0;"><a href="http://hit-school.jimdo.com/%E3%83%93%E3%82%B8%E3%83%8D%E3%82%B9%E3%82%B9%E3%82%AF%E3%83%BC%E3%83%AB/%E3%83%8D%E3%83%83%E3%83%88%E3%82%B7%E3%83%A7%E3%83%83%E3%83%97%E3%82%BB%E3%83%9F%E3%83%8A%E3%83%BC/" target="_blank" style="color: #1B1464; font-size: 18px; font-weight: bold; text-decoration: none;">→　HITビジネススクール　←</a></p>
+		</div>
+		セミナー案内終了 -->
+
+		<img src="images/lp-explanation01.jpg" alt="">
+		<div class="inquiry-box">
+			<div class="inquiry-img">
+			<a href="#form"><img class="inquiry-bt" src="images/inquiry-bt.png" alt="求職者支援訓練に関するお問い合わせはこちらから"></a>
+			</div>
+			<p>募集期間：２０１８年<span class="bosyu-hajime-zen"></span>&nbsp;〜&nbsp;２０１８年<span class="bosyu-owari-zen"></span>まで</p>
+		</div>
+
+		<img src="images/voice.png" alt="">
+		<div class="inquiry-box">
+			<div class="inquiry-img">
+			<a href="#form"><img class="inquiry-bt" src="images/inquiry-bt.png" alt="求職者支援訓練に関するお問い合わせはこちらから"></a>
+			</div>
+			<p>募集期間：２０１８年<span class="bosyu-hajime-zen"></span>&nbsp;〜&nbsp;２０１８年<span class="bosyu-owari-zen"></span>まで</p>
+		</div>
+
+		<div id="form">
+			<div style="margin-left:10px;">
+			<h4>
+				<img src="images/inquiry-bar.jpg" alt="">
+			</h4>
+			<p>
+			見学・説明会を随時開催中。ご連絡の上、スクールへお越しください。<br>お電話もしくは・下記フォームより予約を受け付けております。
+			</p>
+			<hr>
+			<p>【<span class="kaikou"></span>月開講】　訓練生募集締切まで、あと　</p>
+			<div id="CDT">
+				<div id="CDT_sat"></div>
+			</div>
+			</div>
+
+		<form action="mail.php" method="post">
+			<h3 class="inqu">お問い合わせ・学校見学お申込みフォーム</h3>
+					<table>
+						<tr>
+							<th><label for="name">お名前</label></th>
+							<td><span class="req">必須</span><input id="name" type="text" name="お名前"></td>
+						</tr>
+						<tr>
+							<th><label for="ruby">ふりがな</label></th>
+							<td><span class="req">必須</span><input id="ruby" type="text" name="ふりがな"></td>
+						</tr>
+						<tr>
+							<th><label for="email">メールアドレス</label></th>
+							<td><span class="req">必須</span><input id="email" type="text" name="メールアドレス"></td>
+						</tr>
+						<tr>
+							<th><label for="email2">メールアドレス（確認）</label></th>
+							<td><span class="req">必須</span><input id="email2" type="text" name="メールアドレス（確認用）"></td>
+						</tr>
+						<tr>
+							<th><label for="tel">電話番号</label></th>
+							<td><span class="any">任意</span><input id="tel" type="text" name="電話番号" size="11"></td>
+						</tr>
+						<tr>
+							<th>当スクールを知ったきっかけ</th>
+							<td class="clear">
+								<span class="any">任意</span>
+								<div class="cueList">
+									<span class="checkbox"><input id="introduce" type="checkbox" name="当スクールを知ったきっかけ[]" value="紹介"><label for="introduce">紹介</label></span>
+									<span class="checkbox"><input id="handout" type="checkbox" name="当スクールを知ったきっかけ[]" value="チラシ・ポスター"><label for="handout">チラシ・ポスター</label></span>
+									<span class="checkbox"><input id="event" type="checkbox" name="当スクールを知ったきっかけ[]" value="イベント・相談会"><label for="event">イベント・相談会</label></span><br>
+									<span class="checkbox"><input id="web" type="checkbox" name="当スクールを知ったきっかけ[]" value="ネット広告"><label for="web">ネット広告</label></span>
+									<span class="checkbox"><input id="search" type="checkbox" name="当スクールを知ったきっかけ[]" value="検索エンジン"><label for="search">検索エンジン</label></span>
+									<p>
+										<input type="checkbox" id="other" name="当スクールを知ったきっかけ[]" value="その他"><label for="other">その他&ensp;:</label>
+										<input type="text" name="当スクールを知ったきっかけ[]">
+									</p>
+								</div>
+								<!--#cueList END-->
+							</td>
+						</tr>
+						<tr>
+							<th>お問い合せ内容</th>
+							<td><span class="req">必須</span><textarea name="お問い合わせ内容" cols="30" rows="10" tabindex="1"></textarea></td>
+						</tr>
+						<tr>
+							<th>個人情報の取り扱いについて</th>
+							<td>
+								<p class="pv">個人情報の取り扱いについては、以下に記載しておりますプライバシーポリシーを遵守します。
+									下記の「プライバシーポリシー」に同意いただける場合は、「同意する」にチェックを入れ、お問い合わせ内容をご入力ください。
+								<div class="pvbox">
+									<h4>プライバシーポリシー</h4>
+										<p class="pv-det">株式会社阪神インダストリアルテクノロジーは、個人情報の保護に関する法令等を遵守するとともに、以下にプライバシーポリシーを定め、全ての役員、社員、協働者に周知徹底を図ることにより、個人情報の適正な取り扱いに努めてまいります。</p>
+										<h4>1.法令の遵守</h4>
+										<p class="pv-det">当社は個人情報を取り扱うにあたり、個人情報の保護に関する法律および関係する法令、ガイドラインを遵守します。</p>
+										<h4>2.個人情報の収集</h4>
+										<p class="pv-det">当社は、個人情報をご提供いただく場合、個人情報の利用目的の公表またはご本人への通知により、その利用目的を明らかにいたします。ご提供いただいた個人情報の利用目的を変更する場合は、変更後の利用目的を公表、またはご本人へ通知いたします。</p>
+										<h4>3.個人情報の管理・保護</h4>
+										<p class="pv-det">ご提供いただいた個人情報は、管理責任者が適切な管理を行なうとともに、漏洩や紛失等の防止に努めます。また、外部からの不正アクセスや破壊、改ざん等の危険に対して安全対策を実施し、個人情報の保護に努めます。</p>
+										<h4>4.個人情報の利用</h4>
+										<p class="pv-det">当社は、ご本人の同意を得ないで、利用目的の達成に必要な範囲を超えて個人情報を取り扱いいたしません。</p>
+										<h4>5.個人情報の第三者への提供</h4>
+										<p class="pv-det">当社は、ご本人の同意を得ないで、業務委託先以外の第三者に開示、提供することはありません。ただし、法令により開示を求められた場合を除きます。</p>
+										<h4>6.個人情報の開示・訂正等</h4>
+										<p class="pv-det">個人情報につき開示、訂正等の請求があった場合は、その請求がご本人またはその代理人によるものであることを確認した上で、必要かつ合理的な範囲で対応いたします。</p>
+										<h4>7.社内体制の整備</h4>
+										<p class="pv-det">当社は、個人情報の適正な取り扱いを目的とした体制の整備および社内の教育の継続的実施に努めてまいります。<br>
+											個人情報に関するお問い合わせ先<br>
+											〒540-0033 大阪府大阪市中央区石町１丁目２−９ シルバービル２階<br>
+										株式会社阪神インダストリアルテクノロジー 事務室</p>
+										<p>TEL：06-6940-0566(受付時間 10：00〜17：00)<br>
+											土・日・祝祭日・夏季休暇・年末年始は、翌営業日以降の対応とさせていただきます。</p>
+							</div>
+							<p class="pv"><span class="req">必須</span><span class="checkbox"><input id="agree" type="checkbox" name="個人情報の取り扱いについて[]" value="同意する"><label for="agree">同意する</label></span></p>
+
+						</td>
+						</tr>
+
+					</table>
+					<?php
+					  if(isset($_GET['id'])) {
+					      $id = $_GET['id'];
+					  }
+					?>
+					<input id="アフィリエイトID" type="hidden" name="アフィリエイトID" value="<?php echo $id; ?>">
+
+					<input type="submit" class="submitBtn"  tabindex="2" value="内容を確認する">
+				</form>
+		</div>
+		<footer>
+		copyright(c)2013 hanshin Industrial Technology All Rights Reserved.
+		</footer>
+	</div>
+	<!--page-contents　END-->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-54343377-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<!-- Google Code for 2015&#24180;5&#26376;&#38283;&#35611; Conversion Page -->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 998807966;
+var google_conversion_language = "en";
+var google_conversion_format = "3";
+var google_conversion_color = "ffffff";
+var google_conversion_label = "nbvICODI7FoQnrOi3AM";
+var google_remarketing_only = false;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/998807966/?label=nbvICODI7FoQnrOi3AM&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>
+
+</body>
+</html>
---
+++ lp/js/base.js
@@ -0,0 +1,38 @@
+﻿function CountdownTimer(elm,tl,mes){
+ this.initialize.apply(this,arguments);
+}
+CountdownTimer.prototype={
+ initialize:function(elm,tl,mes) {
+  this.elem = document.getElementById(elm);
+  this.tl = tl;
+  this.mes = mes;
+ },countDown:function(){
+  var timer='';
+  var today=new Date();
+  var day=Math.floor((this.tl-today)/(24*60*60*1000));
+  var hour=Math.floor(((this.tl-today)%(24*60*60*1000))/(60*60*1000));
+  var min=Math.floor(((this.tl-today)%(24*60*60*1000))/(60*1000))%60;
+  var sec=Math.floor(((this.tl-today)%(24*60*60*1000))/1000)%60%60;
+  var milli=Math.floor(((this.tl-today)%(24*60*60*1000))/10)%100;
+  var me=this;
+
+  if( ( this.tl - today ) > 0 ){
+   if (day) timer += '<span class="day">'+day+'日</span>';
+   if (hour) timer += '<span class="hour">'+hour+'時間</span>';
+   timer += '<span class="min">'+this.addZero(min)+'分</span><span class="sec">'+this.addZero(sec)+'秒</span><span class="milli">'+this.addZero(milli)+'</span>';
+   this.elem.innerHTML = timer;
+   tid = setTimeout( function(){me.countDown();},10 );
+  }else{
+   this.elem.innerHTML = this.mes;
+   return;
+  }
+ },addZero:function(num){ return ('0'+num).slice(-2); }
+}
+function CDT(){
+ var tl = new Date(COUNTDOWN);
+ var timer = new CountdownTimer('CDT',tl,'終了しました');
+ timer.countDown();
+}
+window.onload=function(){
+ CDT();
+}
